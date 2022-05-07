182dec70993436dafa732b242ef4860e6827f18e
Kirill Koldrysh <kirill.koldrysh@redbit.cz>
2022-05-07 17:28:14 +0200

    Create Hello component. Use component for simple Hello App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-const App = () => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
-    <h1>Hello React</h1>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = "Peter";
+  const age = 11;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 };
