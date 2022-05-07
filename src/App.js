6bb1960e0f64e17594bf291bedde4f8dee3bba1a
Kirill Koldrysh <kirill.koldrysh@redbit.cz>
2022-05-07 18:24:21 +0200

    create simple counter with using React State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
   return (
     <div>
-    <h1>Hello React</h1>
+      <Display counter={counter} />
+      <Button text="minus" onClick={decreaseByOne} />
+      <Button text="zero" onClick={setToZero} />
+      <Button text="plus" onClick={increaseByOne} />
     </div>
   );
 };
