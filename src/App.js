b6eb0c9459aaffcb0ca6c7a25755500662acf53a
kirill.koldrysh <kirill.koldrysh@redbit.cz>
2022-07-04 23:49:47 +0200

    Create skeleton of Phonebook app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-const App = () => {
+import { useState } from 'react';
+
+const App = (props) => {
+  const [persons, setPersons] = useState(props.persons);
+  const [newName, setNewName] = useState('');
+
   return (
     <div>
-    <h1>Hello React</h1>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+
+      {
+        persons.map(
+          person => <li key={person.id}>{person.name}</li>
+        )
+      }
+      </ul>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const persons = [
+  {
+    id: 1,
+    name: 'Arthur the King'
+  },
+  {
+    id: 2,
+    name: 'Lancelot'
+  },
+  {
+    id: 3,
+    name: 'Uter'
+  },
+];
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App persons={persons} />);
