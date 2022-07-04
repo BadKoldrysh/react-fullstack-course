20966b409d2d9b470d0d24cea6c34eacb09c7657
kirill.koldrysh <kirill.koldrysh@redbit.cz>
2022-07-04 23:25:25 +0200

    Add an opportunity of adding new notes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { useState } from 'react';
 import Note from './components/Note';
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note...');
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
   return (
     <div>
       <h2>Notes App</h2>
-        <ul>
-          {notes.map(note => <Note key={note.id} note={note} />)}
-        </ul>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
