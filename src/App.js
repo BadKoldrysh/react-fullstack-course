7c684df1d8b750892902a82200735be52f5cf109
kirill.koldrysh <kirill.koldrysh@redbit.cz>
2022-07-05 21:11:35 +0200

    Add searching field

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = (props) => {
   const [persons, setPersons] = useState(props.persons);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [foundPersons, setFoundPersons] = useState(persons);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -15,13 +17,16 @@ const App = (props) => {
 
     const existingPersons = persons.filter(person => person.name === newName);
     if (existingPersons.length !== 0) {
-      alert(newName + 'is already added to phonebook');
+      alert(newName + ' is already added to phonebook');
       return;
     }
 
-    setPersons(persons.concat(personObject));
+    const newPersonsList = persons.concat(personObject);
+    setPersons(newPersonsList);
     setNewName('');
     setNewNumber('');
+    setFoundPersons(newPersonsList);
+    setSearchInput('');
   };
 
   const handleOnChange = (event) => {
@@ -32,9 +37,22 @@ const App = (props) => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchOnChange = (event) => {
+    const found = persons.filter((person) => {
+      const regexp = new RegExp(event.target.value, 'i');
+
+      return person.name.search(regexp) !== -1;
+    });
+
+    setFoundPersons(found);
+    setSearchInput(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={searchInput} onChange={handleSearchOnChange} /></div>
+      <h2>add a new person</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleOnChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberOnChange} /></div>
@@ -46,7 +64,7 @@ const App = (props) => {
       <h2>Numbers</h2>
       <ul>
       {
-        persons.map(
+        foundPersons.map(
           person => <Person key={person.name} person={person} />
         )
       }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const persons = [
-  {
-    name: 'Arthur the King',
-    number: '+420775090133'
-  },
+  { name: 'Arthur the King', number: '+420775090133' },
+  { name: 'Adam West', number: '+420771290133' },
+  { name: 'Bedrich Smetana', number: '+420778990621' },
+  { name: 'Tom Waits', number: '+38077399062' },
+  { name: 'Walter Paul', number: '+38057891062' },
 ];
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App persons={persons} />);
